ea327af0004f244b482f62b6c523216b4d406bfc
Miguel Alvarez Lesmes <alvarez.lesmes.miguel@gmail.com>
2023-05-28 22:30:26 +0800

    Move the API Keys to a .env file

---
+++ README.md
@@ -0,0 +1,10 @@
+# LangChain Lab
+My first take on the LangChain library
+
+A small set of examples that utilise the LangChain library
+
+# Setup
+
+1. Create a `.env` file
+2. Add the necessary API Keys as `API_KEY_NAME=VALUE`. Make sure you use the same `API_KEY_NAME` 
+3. that is being used on the examples 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from langchain.llms import OpenAI
 import os
-
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
 
 def main():
-    os.environ['OPENAI_API_KEY'] = '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'
+    # Load API keys from environment variables
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
     llm = OpenAI()
     text = "What would be a good company name for an AI powered platform that helps user to do market research?"
     print(llm(text))
