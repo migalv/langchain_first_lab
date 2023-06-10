e26ddb95ebe8ba286b20a83c5ca109c0f8ea2957
Miguel Alvarez Lesmes <alvarez.lesmes.miguel@gmail.com>
2023-06-10 17:52:22 +0800

    Summarise YouTube transcript with OpenAI

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
+from youtube_genie import summarise_transcript_with_open_ai
+
+
 def main():
     # Load API keys from environment variables
     load_dotenv()
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-    llm = OpenAI()
-    text = "What would be a good company name for an AI powered platform that helps user to do market research?"
-    print(llm(text))
+    summarise_transcript_with_open_ai(
+        youtube_url="https://www.youtube.com/watch?v=qaPMdcCqtWk&list=PLqZXAkvF1bPNQER9mLmDbntNfSpzdDIU5&index=20&t=5s&ab_channel=DataIndependent"
+    )
+
+    # llm = OpenAI()
+    # text = "What would be a good company name for an AI powered platform that helps user to do market research?"
+    # print(llm(text))
 
 
 if __name__ == '__main__':
